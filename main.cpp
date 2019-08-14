d0a3cafea549ff4aa3db6e6c774a4b3a0e2ea2aa
hehao1993 <347428365@qq.com>
2019-08-14 18:03:22 +0800

    try catch

--- main.cpp
+++ main.cpp
@@ -1,56 +1,28 @@
 #include <iostream>
-
 using namespace std;
 
-class Box
+double division(int a, int b)
 {
-	double width;
-public:
-	friend void printWidth(Box box);
-	friend class BigBox;
-	void setWidth(double wid);
-};
-
-class BigBox
-{
-public:
-	void Print(int width, Box &box)
+	if (b == 0)
 	{
-		// BigBox是Box的友元类，它可以直接访问Box类的任何成员
-		//box.setWidth(width);
-		box.width = width;
-		cout << "Width of box : " << box.width << endl;
+		throw "Division by zero condition!";
 	}
-};
-
-// 成员函数定义
-void Box::setWidth(double wid)
-{
-	width = wid;
+	return (a / b);
 }
 
-// 请注意：printWidth() 不是任何类的成员函数
-void printWidth(Box box)
-{
-	/* 因为 printWidth() 是 Box 的友元，它可以直接访问该类的任何成员 */
-	cout << "Width of box : " << box.width << endl;
-}
-
-// 程序的主函数
 int main()
 {
-	Box box;
-	BigBox big;
+	int x = 50;
+	int y = 0;
+	double z = 0;
 
-	// 使用成员函数设置宽度
-	box.setWidth(10.0);
+	try {
+		z = division(x, y);
+		cout << z << endl;
+	}
+	catch (const char* msg) {
+		cerr << msg << endl;
+	}
 
-	// 使用友元函数输出宽度
-	printWidth(box);
-
-	// 使用友元类中的方法设置宽度
-	big.Print(20, box);
-
-	printWidth(box);
 	return 0;
 }
