dd5057b0e45e73c164d9890c46568e9df74d4f15
hehao1993 <347428365@qq.com>
2019-08-20 18:17:45 +0800

    读取文件内容赋值给变量

--- main.cpp
+++ main.cpp
@@ -1,33 +1,21 @@
+#include <string>
+#include <fstream>
+#include <sstream>
 #include <iostream>
-#include <csignal>
-#include <windows.h>
-
 using namespace std;
-
-void signalHandler(int signum)
-{
-	cout << "Interrupt signal (" << signum << ") received.\n";
-
-	// 清理并关闭
-	// 终止程序 
-
-	exit(signum);
-
-}
 
 int main()
 {
-	int i = 0;
-	// 注册信号 SIGINT 和信号处理程序
-	signal(SIGINT, signalHandler);
-
-	while (++i) {
-		cout << "Going to sleep...." << endl;
-		if (i == 3) {
-			raise(SIGINT);
-		}
-		Sleep(1);
-	}
-
+	string data;
+	ifstream infile;
+	infile.open("afile.dat");
+	ostringstream ss;
+	ss << infile.rdbuf();
+	string str = ss.str();
+	cout << str << endl;
+	cout << "=======================" << endl;
+	const char* cstr = str.c_str();
+	cout << cstr << endl;
+	infile.close();
 	return 0;
 }
